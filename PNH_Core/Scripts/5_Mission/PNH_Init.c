4ffc9da019c5ae2ed571286e486fdb90fd766f84
PinheiroW <152659864+PinheiroW@users.noreply.github.com>
2026-02-14 15:43:15 -0300

    att
    att .init

--- PNH_Core/Scripts/5_Mission/PNH_Init.c
+++ PNH_Core/Scripts/5_Mission/PNH_Init.c
@@ -1,23 +1,23 @@
 modded class MissionServer
 {
-    // Declara o nosso Cérebro de Missões
+    // 1. Declara o nosso Cérebro de Missões na memória do servidor
     ref PNH_MissionManager m_PNH_MissionManager;
 
     override void OnInit()
     {
         super.OnInit();
         
-        // 1. Carrega as Configurações do Core (CRUCIAL)
+        // Carrega as Configurações do Core
         PNH_CoreConfig.Load();
         
-        // 2. Log de inicialização
+        // Log de inicialização do sistema base
         PNH_Logger.Log("Core", "Sistema PNH Utils e Config carregados.");
         
-        // 3. Inicia o Sistema de Missões PNH
+        // 2. INICIA O SISTEMA DE MISSÕES (O motor liga aqui!)
         m_PNH_MissionManager = new PNH_MissionManager();
     }
 
-    // O "Coração" do Servidor - Bate a cada Frame
+    // 3. O "Coração" do Servidor - Bate a cada Frame do jogo
     override void OnUpdate(float timeslice)
     {
         super.OnUpdate(timeslice);
