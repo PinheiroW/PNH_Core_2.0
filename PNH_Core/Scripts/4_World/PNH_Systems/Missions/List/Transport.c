64282d55a719ef7ef54cfe7cbe5d86517ef460ca
PinheiroW <152659864+PinheiroW@users.noreply.github.com>
2026-02-16 23:00:18 -0300

    Update Transport.c

--- PNH_Core/Scripts/4_World/PNH_Systems/Missions/List/Transport.c
+++ PNH_Core/Scripts/4_World/PNH_Systems/Missions/List/Transport.c
@@ -25,10 +25,12 @@ class TransportMission extends PNH_MissionBase
         if (FileExist(caminhoJson)) JsonFileLoader<PNH_MissionData_Transport>.JsonLoadFile(caminhoJson, m_Config);
 
         m_MissionTimeout = m_Config.TempoLimiteSegundos;
-        m_MissionInformant = m_Config.Lore.Informante;
         
-        if (m_Config.Lore.MensagensRadio && m_Config.Lore.MensagensRadio.Count() > 0)
-            m_MissionMessage1 = m_Config.Lore.MensagensRadio[0];
+        if (m_Config.Lore) {
+            m_MissionInformant = m_Config.Lore.Informante;
+            if (m_Config.Lore.MensagensRadio && m_Config.Lore.MensagensRadio.Count() > 0)
+                m_MissionMessage1 = m_Config.Lore.MensagensRadio[0];
+        }
     }
 
     int ContarItens(PlayerBase player, string classeAlvo)
@@ -40,7 +42,7 @@ class TransportMission extends PNH_MissionBase
             if (itemsArray[i] && itemsArray[i].GetType() == classeAlvo) total++;
         }
         
-        if (m_MissionVehicle) {
+        if (m_MissionVehicle && m_MissionVehicle.GetInventory()) {
             array<EntityAI> carItems = new array<EntityAI>;
             m_MissionVehicle.GetInventory().EnumerateInventory(InventoryTraversalType.PREORDER, carItems);
             for (int j = 0; j < carItems.Count(); j++) {
@@ -52,7 +54,8 @@ class TransportMission extends PNH_MissionBase
 
     override void PlayerChecks(PlayerBase player)
     {
-        if (m_CurrentPhase == 4) return;
+        if (m_CurrentPhase == 4 || !m_Config || !m_Config.Lore) return;
+
         vector playerPos = player.GetPosition();
 
         // --- FASE A: VYBOR ---
@@ -60,7 +63,7 @@ class TransportMission extends PNH_MissionBase
         {
             float distA = vector.Distance(playerPos, m_Config.PosicaoVeiculo.ToVector());
             if (!m_AWarnO && distA <= 150.0) { m_AWarnO = true; EnviarAviso(m_MissionInformant, m_Config.Lore.MensagemAproximacao + m_Config.CidadeFaseA + "."); }
-            if (!m_AWarnI && distA <= 20.0)  { m_AWarnI = true; EnviarAviso(m_MissionInformant, m_Config.Lore.MensagemNoObjetivo + " Monte o veículo."); }
+            if (!m_AWarnI && distA <= 20.0)  { m_AWarnI = true; EnviarAviso(m_MissionInformant, m_Config.Lore.MensagemNoObjetivo + " Monte as peças e pneus no veículo."); }
 
             if (m_MissionVehicle && m_MissionVehicle.EngineIsOn())
             {
@@ -87,6 +90,7 @@ class TransportMission extends PNH_MissionBase
             if (ContarItens(player, m_Config.ClasseMaleta) >= 3 && qtdLivroArte >= 1) {
                 m_CurrentPhase = 2;
                 EnviarAviso(m_MissionInformant, m_Config.Lore.MensagemFaseC);
+                PNH_Logger.Log("Missões", "[PNH] Fase B Concluída.");
             }
         }
         // --- FASE C: IGREJA ---
@@ -94,7 +98,7 @@ class TransportMission extends PNH_MissionBase
         {
             float distC = vector.Distance(playerPos, m_Config.PosicaoCorpo.ToVector());
             if (!m_CWarnO && distC <= 150.0) { m_CWarnO = true; EnviarAviso(m_MissionInformant, m_Config.Lore.MensagemAproximacao + m_Config.CidadeFaseC + "."); }
-            if (!m_CWarnI && distC <= 20.0)  { m_CWarnI = true; EnviarAviso(m_MissionInformant, "Igreja visualizada. Use a gazua para entrar!"); }
+            if (!m_CWarnI && distC <= 20.0)  { m_CWarnI = true; EnviarAviso(m_MissionInformant, "Igreja visualizada. O contato pode estar lá dentro."); }
 
             int qtdBiblia = ContarItens(player, m_Config.ClasseLivroBiblia);
             
@@ -107,6 +111,7 @@ class TransportMission extends PNH_MissionBase
             if (m_AmbushChurchTriggered && qtdBiblia >= 1 && distC > 100.0) {
                 m_CurrentPhase = 3;
                 EnviarAviso(m_MissionInformant, m_Config.Lore.MensagemFaseD + m_Config.CidadeFaseD + ".");
+                PNH_Logger.Log("Missões", "[PNH] Fase C Concluída.");
             }
         }
         // --- FASE D: ALTAR ---
@@ -122,17 +127,23 @@ class TransportMission extends PNH_MissionBase
                     if (ContarItens(player, m_Config.ClasseMaleta) >= 3 && ContarItens(player, m_Config.ClasseLivroArteDaGuerra) >= 1 && ContarItens(player, m_Config.ClasseLivroBiblia) >= 1) {
                         m_CurrentPhase = 4;
                         GetGame().ObjectDelete(m_MissionVehicle);
+                        
                         vector rPos = m_Config.PosicaoBarril.ToVector();
                         m_RewardContainer = ItemBase.Cast(GetGame().CreateObject(m_Config.Recompensas.Container, rPos, false, false, true));
                         if (m_RewardContainer) {
-                            auto loadout = m_Config.Recompensas.Loadouts.GetRandomElement();
-                            for (int l = 0; l < loadout.Itens.Count(); l++) m_RewardContainer.GetInventory().CreateInInventory(loadout.Itens[l]);
+                            if (m_Config.Recompensas && m_Config.Recompensas.Loadouts && m_Config.Recompensas.Loadouts.Count() > 0) {
+                                auto loadout = m_Config.Recompensas.Loadouts.GetRandomElement();
+                                for (int l = 0; l < loadout.Itens.Count(); l++) m_RewardContainer.GetInventory().CreateInInventory(loadout.Itens[l]);
+                            }
                             m_RewardContainer.Close();
                             m_MissionObjects.Insert(m_RewardContainer);
                         }
+                        
                         EnviarAviso("Comando PNH", m_Config.Lore.MensagemVitoria);
                         PNH_MissionSettingsData config = PNH_MissionSettings.GetData();
-                        m_MissionTimeout = m_MissionTime + config.ConfiguracoesGerais.TempoLimpezaSegundos;
+                        if (config && config.ConfiguracoesGerais) {
+                            m_MissionTimeout = m_MissionTime + config.ConfiguracoesGerais.TempoLimpezaSegundos;
+                        }
                     }
                 }
             }
@@ -143,57 +154,59 @@ class TransportMission extends PNH_MissionBase
     {
         if (!m_Config || !m_Config.Ativa) return false;
 
-        // FASE A
+        // FASE A - O HUMVEE SEM RODAS
         m_MissionVehicle = Car.Cast(GetGame().CreateObject(m_Config.ClasseVeiculo, m_Config.PosicaoVeiculo.ToVector()));
         if (m_MissionVehicle) {
             m_MissionVehicle.SetOrientation(m_Config.OrientacaoVeiculo.ToVector());
-            m_MissionVehicle.GetInventory().CreateAttachment("HatchbackDoors_Driver");
-            m_MissionVehicle.GetInventory().CreateAttachment("HatchbackDoors_CoDriver");
-            m_MissionVehicle.GetInventory().CreateAttachment("HatchbackHood");
-            for(int w=0; w<4; w++) m_MissionVehicle.GetInventory().CreateAttachment("HatchbackWheel");
+            if (m_MissionVehicle.GetInventory()) {
+                // Monta a lataria do Humvee, MAS DEIXA SEM RODAS!
+                m_MissionVehicle.GetInventory().CreateAttachment("Offroad_02_Door_1_1");
+                m_MissionVehicle.GetInventory().CreateAttachment("Offroad_02_Door_1_2");
+                m_MissionVehicle.GetInventory().CreateAttachment("Offroad_02_Door_2_1");
+                m_MissionVehicle.GetInventory().CreateAttachment("Offroad_02_Door_2_2");
+                m_MissionVehicle.GetInventory().CreateAttachment("Offroad_02_Hood");
+                m_MissionVehicle.GetInventory().CreateAttachment("Offroad_02_Trunk");
+            }
             m_MissionObjects.Insert(m_MissionVehicle);
         }
-        for (int i = 0; i < m_Config.PecasVeiculo.Count(); i++) {
-            PNH_MissionSettings_ItemPos p = m_Config.PecasVeiculo[i];
-            Object peca = GetGame().CreateObject(p.Classe, p.Posicao.ToVector());
-            if (peca) { peca.SetOrientation(p.Orientacao.ToVector()); m_MissionObjects.Insert(peca); }
+        
+        if (m_Config.PecasVeiculo) {
+            for (int i = 0; i < m_Config.PecasVeiculo.Count(); i++) {
+                PNH_MissionSettings_ItemPos p = m_Config.PecasVeiculo[i];
+                Object peca = GetGame().CreateObject(p.Classe, p.Posicao.ToVector());
+                if (peca) { peca.SetOrientation(p.Orientacao.ToVector()); m_MissionObjects.Insert(peca); }
+            }
         }
         SpawnZumbis(m_Config.ZumbisFaseA, m_Config.PosicaoVeiculo.ToVector(), 20.0);
 
         // FASE B
-        for (int m = 0; m < m_Config.PosicoesMaletas.Count(); m++) {
-            PNH_MissionSettings_ItemPos mal = m_Config.PosicoesMaletas[m];
-            Object maleta = GetGame().CreateObject(mal.Classe, mal.Posicao.ToVector());
-            if (maleta) { maleta.SetOrientation(mal.Orientacao.ToVector()); m_MissionObjects.Insert(maleta); }
+        if (m_Config.PosicoesMaletas) {
+            for (int m = 0; m < m_Config.PosicoesMaletas.Count(); m++) {
+                PNH_MissionSettings_ItemPos mal = m_Config.PosicoesMaletas[m];
+                Object maleta = GetGame().CreateObject(mal.Classe, mal.Posicao.ToVector());
+                if (maleta) { maleta.SetOrientation(mal.Orientacao.ToVector()); m_MissionObjects.Insert(maleta); }
+            }
         }
         Object livroA = GetGame().CreateObject(m_Config.ClasseLivroArteDaGuerra, m_Config.PosicaoLivroArteDaGuerra.ToVector());
         if (livroA) { livroA.SetOrientation(m_Config.OrientacaoLivroArteDaGuerra.ToVector()); m_MissionObjects.Insert(livroA); }
+        
         Object gazua = GetGame().CreateObject(m_Config.ClasseFerramenta, m_Config.PosicaoFerramenta.ToVector());
         if (gazua) { gazua.SetOrientation(m_Config.OrientacaoFerramenta.ToVector()); m_MissionObjects.Insert(gazua); }
+
         SpawnZumbis(m_Config.ZumbisFaseB, m_Config.PosicaoGalpaoFaseB.ToVector(), 25.0);
 
         // FASE C
-        Object churchObj = null;
-        GetGame().GetObjectsAtPosition(m_Config.PosicaoCorpo.ToVector(), 30.0, m_ObjectList, m_ObjectCargoList);
-        for (int b = 0; b < m_ObjectList.Count(); b++) {
-            if (m_ObjectList[b].GetType().Contains("Church")) { churchObj = m_ObjectList[b]; break; }
-        }
-        if (churchObj) {
-            Building church = Building.Cast(churchObj);
-            for (int k = 0; k < 10; k++) {
-                if (church.IsDoorOpen(k)) church.CloseDoor(k);
-                if (!church.IsDoorLocked(k)) church.LockDoor(k);
-            }
-        }
         m_DeadPriest = GetGame().CreateObject(m_Config.ClasseCorpo, m_Config.PosicaoCorpo.ToVector(), false, false, true);
         if (m_DeadPriest) {
             m_DeadPriest.SetOrientation(m_Config.OrientacaoCorpo.ToVector());
             EntityAI priestEnt = EntityAI.Cast(m_DeadPriest);
-            priestEnt.SetHealth("", "", 10);
+            if (priestEnt) priestEnt.SetHealth("", "", 10);
             m_MissionObjects.Insert(m_DeadPriest);
+            
             vector zKillPos = m_Config.PosicaoZumbiAssassino.ToVector();
             m_MissionAIs.Insert(GetGame().CreateObject(m_Config.ClasseZumbiAssassino, zKillPos, false, true, true));
         }
+        
         Object livroB = GetGame().CreateObject(m_Config.ClasseLivroBiblia, m_Config.PosicaoLivroBiblia.ToVector());
         if (livroB) { livroB.SetOrientation(m_Config.OrientacaoLivroBiblia.ToVector()); m_MissionObjects.Insert(livroB); }
 
@@ -202,11 +215,15 @@ class TransportMission extends PNH_MissionBase
         if (m_NPC) {
             m_NPC.SetOrientation(m_Config.OrientacaoNPC.ToVector());
             EntityAI npcEnt = EntityAI.Cast(m_NPC);
-            for (int r = 0; r < m_Config.RoupasNPC.Count(); r++) npcEnt.GetInventory().CreateInInventory(m_Config.RoupasNPC[r]);
+            if (npcEnt && m_Config.RoupasNPC) {
+                for (int r = 0; r < m_Config.RoupasNPC.Count(); r++) {
+                    npcEnt.GetInventory().CreateInInventory(m_Config.RoupasNPC[r]);
+                }
+            }
             m_MissionObjects.Insert(m_NPC);
         }
 
-        PNH_Logger.Log("Missões", "[PNH SYSTEM] Operação Prometeu Iniciada.");
+        PNH_Logger.Log("Missões", "[PNH SYSTEM] Operação Prometeu Iniciada (Mecânica Hardcore).");
         return true;
     }
     
@@ -219,7 +236,7 @@ class TransportMission extends PNH_MissionBase
     }
 
     void SpawnZumbis(int qtd, vector pos, float raio) {
-        if (m_Config.Dificuldade && m_Config.Dificuldade.ClassesZumbis.Count() > 0) {
+        if (m_Config && m_Config.Dificuldade && m_Config.Dificuldade.ClassesZumbis && m_Config.Dificuldade.ClassesZumbis.Count() > 0) {
             for (int z = 0; z < qtd; z++) {
                 vector zPos = pos + Vector(Math.RandomFloat(-raio, raio), 0, Math.RandomFloat(-raio, raio));
                 zPos[1] = GetGame().SurfaceY(zPos[0], zPos[2]);
@@ -230,7 +247,10 @@ class TransportMission extends PNH_MissionBase
 
     void EnviarAviso(string emissor, string msg) {
         PNH_MissionSettingsData config = PNH_MissionSettings.GetData();
-        if (config.ConfiguracoesGerais.UsarPDA) GetRPCManager().SendRPC("[GearPDA] ", "SendGlobalMessage", new Param2<string, string>(emissor, msg), true);
-        else PNH_Utils.SendMessageToAll("[" + emissor + "] " + msg);
+        if (config && config.ConfiguracoesGerais && config.ConfiguracoesGerais.UsarPDA) {
+            GetRPCManager().SendRPC("[GearPDA] ", "SendGlobalMessage", new Param2<string, string>(emissor, msg), true);
+        } else {
+            PNH_Utils.SendMessageToAll("[" + emissor + "] " + msg);
+        }
     }
 }
